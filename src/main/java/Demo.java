7f0ebfeccb8826dc7685a46c03dad316a67dcd6f
sedatekmel <sedatekmel_91@hotmail.com>
2025-09-21 15:22:38 +0300

    Commit from Lithuania

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,8 +1,11 @@
+import java.sql.SQLOutput;
+
 public class Demo {
 
     public static void main(String[] args) {
 
         System.out.println("Hello Github");
         System.out.println("Hi Github");
+        System.out.println("Labas rytas github");
     }
 }
